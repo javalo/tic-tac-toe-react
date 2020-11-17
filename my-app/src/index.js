6063c864e2f71437110bdacecfce5057e0e4b007
javalo <lo.cheikhna@gmail.com>
2020-11-17 21:52:26 +0100

    Declaring a Winner

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -44,8 +44,16 @@ import './index.css';
   
     render() {
 
-      const status = 'Next player:'+(this.state.xIsNext ? 'X' : '0');
+     // const status = 'Next player:'+(this.state.xIsNext ? 'X' : '0');
   
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -86,7 +94,31 @@ import './index.css';
       );
     }
   }
+
+
+
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
+
   // ========================================
   
   ReactDOM.render(
